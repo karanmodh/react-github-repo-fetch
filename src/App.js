fed1091f87a35b0aceaff0db65ec9645994e8f85
Karan Modh <karan.modh@navi.com>
2021-07-27 14:05:22 +0530

    renamed files and removed extra unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Greetings from './components/Greetings';
+import Header from './components/Header';
 import Search from './components/Search';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Greetings name="Karan"/>
+    <div className="App" style={{textAlign: "center"}}>
+      <Header text="Github Repo Search"/>
       <Search />
     </div>
   );
--- src/components/Greetings.js
+++
@@ -1,9 +0,0 @@
-function Greetings() {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    );
-}
-
-export default Greetings
